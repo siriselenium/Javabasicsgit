a007fd3ced7391431a87571c40f81181a93ccbd7
siris <siris@DESKTOP-Q05SUNC>
2019-11-15 18:50:28 -0800

    Added code

--- src/classwork/AreaofCircle.java
+++ src/classwork/AreaofCircle.java
@@ -5,7 +5,7 @@ public class AreaofCircle {
 	public static void main(String[] args) {
 		System.out.println("Area of Circle");
 		System.out.println("Area of Circle2");
-
+		System.out.println("Area of Circle3");
 	}
 
 }
